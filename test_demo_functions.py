9474b0a73994b15e1f998f68b5e10f0946c7f652
bednie <88800117+bednie@users.noreply.github.com>
2024-02-19 17:39:07 +0000

    Add demo and tests

---
+++ test_demo_functions.py
@@ -0,0 +1,13 @@
+# test_demo_functions.py
+import pytest
+from demo_functions import add, multiply
+
+def test_add():
+    assert add(2, 3) == 5, "Should be 5"
+    assert add(-1, 1) == 0, "Should be 0"
+    assert add(-1, -1) == -2, "Should be -2"
+
+def test_multiply():
+    assert multiply(2, 3) == 6, "Should be 6"
+    assert multiply(-1, 1) == -1, "Should be -1"
+    assert multiply(-1, -1) == 1, "Should be 1"
